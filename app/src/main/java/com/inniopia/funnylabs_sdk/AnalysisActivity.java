5ae332d8e363760459f644f2ce4db27d469aac7f
SuyeongOh <pental_vega@naver.com>
2024-02-06 17:13:07 +0900

    add button layout, listener

--- app/src/main/java/com/inniopia/funnylabs_sdk/AnalysisActivity.java
+++ app/src/main/java/com/inniopia/funnylabs_sdk/AnalysisActivity.java
@@ -1,5 +1,6 @@
 package com.inniopia.funnylabs_sdk;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ViewGroup;
@@ -8,13 +9,21 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.psambit9791.jdsp.signal.Detrend;
 import com.github.psambit9791.jdsp.signal.Smooth;
 import com.github.psambit9791.jdsp.transform.DiscreteFourier;
 import com.inniopia.funnylabs_sdk.bvp.BandPassFilter;
+import com.inniopia.funnylabs_sdk.bvp.ModuleName;
 import com.inniopia.funnylabs_sdk.data.VitalChartData;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,12 +41,8 @@ public class AnalysisActivity extends AppCompatActivity {
             VitalChartData.B_SIGNAL
     };
     private double [][] smoothPixel = new double[3][];
-    private double [] smoothR;
-    private double [] smoothG;
-    private double [] smoothB;
     private double [] currentSignal;
     private static final String[] MODULE_ARRAY = new String[]{
-            "Smoothing",
             "OMIT",
             "Detrend",
             "Band-Pass Filter",
@@ -50,7 +55,10 @@ public class AnalysisActivity extends AppCompatActivity {
     private TextView processTextView;
     private Button resetButton;
 
+    private double hr;
     private boolean isSmooth;
+
+    private boolean isFFTSignal = false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,7 +69,12 @@ public class AnalysisActivity extends AppCompatActivity {
         processTextView = findViewById(R.id.analysis_text_progress);
         resetButton = findViewById(R.id.analysis_button_reset);
 
+        init();
+        initChart("description");
 
+        drawChart(pixelRGB[0], Color.RED);
+        drawChart(pixelRGB[1], Color.GREEN);
+        drawChart(pixelRGB[2], Color.BLUE);
     }
 
     private void init(){
@@ -73,12 +86,16 @@ public class AnalysisActivity extends AppCompatActivity {
         buttonYes.setText("예");
         buttonYes.setOnClickListener(view -> {
            smoothPixel = dspSmoothing(pixelRGB);
+           drawChart(smoothPixel[0], Color.RED);
+           drawChart(smoothPixel[1], Color.GREEN);
+           drawChart(smoothPixel[2], Color.BLUE);
+           initButtonGroup();
         });
         Button buttonNo = new Button(this);
         buttonNo.setLayoutParams(functionLayoutParams);
         buttonNo.setText("아니오");
         buttonNo.setOnClickListener(view -> {
-
+            initButtonGroup();
         });
         functionLayout.addView(buttonYes);
     }
@@ -166,6 +183,117 @@ public class AnalysisActivity extends AppCompatActivity {
         return max_index * frequency_interval * 60;
     }
 
+    private void initButtonGroup(){
+        for (String buttonText : MODULE_ARRAY) {
+            Button button = new Button(this);
+
+            button.setLayoutParams(functionLayoutParams);
+            button.setText(buttonText);
+            button.setOnClickListener(view -> {
+                double[][] targetSignal = new double[3][];
+                if(isSmooth){
+                    targetSignal = smoothPixel;
+                }else{
+                    targetSignal = pixelRGB;
+                }
+                switch (buttonText){
+                    case ModuleName.CORE_OMIT:
+                        currentSignal = coreOmit(targetSignal);
+                        drawChart(currentSignal, Color.BLUE);
+                        break;
+                    case ModuleName.DSP_DETREND:
+                        currentSignal = dspDetrend(currentSignal);
+                        drawChart(currentSignal, Color.BLUE);
+                        break;
+                    case ModuleName.DSP_BPF:
+                        currentSignal = dspBpf(currentSignal);
+                        drawChart(currentSignal, Color.BLUE);
+                        break;
+                    case ModuleName.DSP_FFT:
+                        currentSignal = dspFft(currentSignal);
+                        isFFTSignal = true;
+                        drawChart(currentSignal, Color.BLUE);
+                        break;
+                    case ModuleName.VITAL_HR:
+                        if(isFFTSignal){
+                            hr = getHr(currentSignal);
+                            drawChart(currentSignal, Color.BLUE);
+                        }
+                        break;
+                }
+            });
+            functionLayout.addView(button);
+        }
+    }
+    private void initChart(String description){
+        analysisChart.getDescription().setText(description);
+        analysisChart.getDescription().setEnabled(true);
+        Legend legend = analysisChart.getLegend();
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setFormSize(10);
+        legend.setTextSize(13);
+        legend.setTextColor(Color.parseColor("#A3A3A3"));
+        legend.setOrientation(Legend.LegendOrientation.VERTICAL);
+        legend.setDrawInside(true);
+        legend.setYEntrySpace(3);
+
+        XAxis xAxis = analysisChart.getXAxis();
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); // x축 데이터 표시 위치
+        xAxis.setGranularity(1f);
+        xAxis.setTextSize(14f);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.setSpaceMin(0.1f); // Chart 맨 왼쪽 간격 띄우기
+        xAxis.setSpaceMax(0.1f); // Chart 맨 오른쪽 간격 띄우기
+
+        // YAxis(Right) (왼쪽) - 선 유무, 데이터 최솟값/최댓값, 색상
+        YAxis yAxisLeft = analysisChart.getAxisLeft();
+        yAxisLeft.setDrawAxisLine(false);
+        yAxisLeft.setTextColor(Color.BLACK);
+        yAxisLeft.setDrawAxisLine(false);
+        yAxisLeft.resetAxisMinimum();
+        yAxisLeft.setAxisLineWidth(2);
+
+        // YAxis(Left) (오른쪽) - 선 유무, 데이터 최솟값/최댓값, 색상
+        YAxis yAxis = analysisChart.getAxisRight();
+        yAxis.setDrawLabels(false); // label 삭제
+        yAxis.setTextColor(Color.BLACK);
+        yAxis.setDrawAxisLine(false);
+        yAxis.setAxisLineWidth(2);
+        yAxisLeft.resetAxisMinimum();
+        yAxis.setAxisMaximum((float) 1); // 최댓값
+        yAxis.setGranularity((float) 0.1);
+    }
+    private void drawChart(double[] signal, int lineColor){
+        List<Entry> entryList = new ArrayList<>();
+        for (int i = 0; i < signal.length; i++) {
+            entryList.add(new Entry(i, (float) signal[i]));
+        }
+        LineDataSet dataset = new LineDataSet(entryList, "");
+        dataset.setDrawCircles(false);
+        dataset.setColor(lineColor);
+        LineData data = new LineData(dataset);
+        analysisChart.setData(data);
+        analysisChart.notifyDataSetChanged();
+        analysisChart.invalidate();
+    }
+    private void drawHrChart(double[] signal, int lineColor){
+        List<Entry> entryList = new ArrayList<>();
+        for (int i = 0; i < signal.length; i++) {
+            entryList.add(
+                    new Entry(VitalChartData.FREQUENCY_INTERVAL * 60 * (i + VitalChartData.START_FILTER_INDEX), (float) signal[i]));
+        }
+        LineDataSet dataset = new LineDataSet(entryList, "");
+        dataset.setDrawCircles(false);
+        dataset.setColor(lineColor);
+        LineData data = new LineData(dataset);
+        analysisChart.setData(data);
+        analysisChart.notifyDataSetChanged();
+        analysisChart.invalidate();
+    }
     private final ViewGroup.LayoutParams functionLayoutParams = new ViewGroup.LayoutParams(
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.WRAP_CONTENT);
